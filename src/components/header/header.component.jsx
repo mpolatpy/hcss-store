ec6e562013f56e10fe7551bc9f05dbc1a79ce78d
Murat Polat <mpolatpy@gmail.com>
2021-01-16 22:52:48 -0500

    redux added. Redirect feature is implemented is user is signed in

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import {ReactComponent as Logo } from '../../assets/crown.svg'
 import './header.styles.scss';
 import { auth } from '../../firebase/firebase.utils';
+import { connect } from 'react-redux';
 
 const Header = ({ currentUser }) => (
     <div className='header'>
@@ -16,6 +17,7 @@ const Header = ({ currentUser }) => (
             <Link className='option' to='/shop'>
                 CONTACT
             </Link>
+            
             { currentUser ?
                 (<div className='option' onClick={() => auth.signOut()}>LOGOUT</div>):
                 (<Link className='option' to='/login'>LOGIN</Link>)
@@ -25,4 +27,8 @@ const Header = ({ currentUser }) => (
     </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
