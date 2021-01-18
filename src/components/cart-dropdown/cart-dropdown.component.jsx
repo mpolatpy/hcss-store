3221607be07d750df617da0984d4a96f7407b5a1
Murat Polat <mpolatpy@gmail.com>
2021-01-18 01:12:26 -0500

    reselect library added. cart and user selectors are implemented.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import CartItem from '../cart-item/cart-item.component';
 import CustomButton from '../custom-button/custom-button.component';
 import './cart-dropdown.styles.scss';
+import {selectCartItems} from '../../redux/cart/cart.selectors';
 
 const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
@@ -18,8 +20,8 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 );
 
-const mapStateToProps = ( { cart: { cartItems } } ) => ({
- cartItems
+const mapStateToProps = createStructuredSelector ({
+ cartItems: selectCartItems
 });
 
 export default connect(mapStateToProps)(CartDropdown);
