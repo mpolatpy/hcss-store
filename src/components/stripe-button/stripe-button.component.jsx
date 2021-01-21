acc53f8b9c5a95d06e9e417e167331efdf9da21e
Murat Polat <mpolatpy@gmail.com>
2021-01-20 21:10:10 -0500

    integrated stripe and updated the favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51IBrmjHDP2n5OmIZZUwBXWzCXh1CYr3H6mtek6uCdkTzDh1EVVpFeKmPEwxpTKDEEQpByFxhLWBc6p7DzQP1Ivfz00CP2E6u0o';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Succesful!');
+    };
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
