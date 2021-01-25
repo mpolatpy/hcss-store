5c13339faabecbb774c22d1ff14651256f95abb7
Murat Polat <mpolatpy@gmail.com>
2021-01-24 20:02:50 -0500

    shop page is refactored using container pattern

---
+++ src/components/collections-overview/collections-overview.container.jsx
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { createStructuredSelector } from 'reselect';
+
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
+import { selectIsCollectionFetching } from '../../redux/shop/shop.selectors';
+
+const mapStateToProps = createStructuredSelector({
+    isLoading: selectIsCollectionFetching
+});
+
+const CollectionsOverviewContainer = compose( 
+    connect(mapStateToProps),
+    WithSpinner
+)(CollectionsOverview);
+
+export default CollectionsOverviewContainer;
+
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -17,3 +17,13 @@ export const selectCollection = collectionId => createSelector(
     [selectShopCollections], 
     collections => collections ? collections[collectionId] : null
 );
+
+export const selectIsCollectionFetching = createSelector ( 
+    [selectShop],
+    shop => shop.isFetching
+);
+
+export const selectIsCollectionsLoaded = createSelector ( 
+    [selectShop],
+    shop => !!shop.collections
+);
