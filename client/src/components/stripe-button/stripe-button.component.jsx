9ead2fda42eb805d1630035489eb4ff7e65ff80b
Murat Polat <mpolatpy@gmail.com>
2021-02-16 02:02:47 -0500

    client folder created. express added for backend

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51IBrmjHDP2n5OmIZZUwBXWzCXh1CYr3H6mtek6uCdkTzDh1EVVpFeKmPEwxpTKDEEQpByFxhLWBc6p7DzQP1Ivfz00CP2E6u0o';
 
     const onToken = token => {
-        console.log(token);
-        alert('Payment Succesful!');
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        })
+        .then((response) => {
+            alert('Payment successfull');
+        })
+        .catch((err) => {
+            console.log('Payment error', JSON.parse(err));
+            alert('There was an issue with your payment. Please use the provided credit card')
+        });
     };
 
     return (
